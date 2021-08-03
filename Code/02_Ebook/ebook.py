11f02b2a90a68f12b4a319dd0832cd64316b1055
Timo Günther <timo.guenther@stud-mail.uni-wuerzburg.de>
2021-08-03 14:37:34 +0200

    Show text, write text und save changes

--- Code/02_Ebook/ebook.py
+++ Code/02_Ebook/ebook.py
@@ -1,44 +1,75 @@
 import tkinter as tk
-from glob import glob
+import glob
 
 
-class Notepad:
+class Ebook:
     fullscreen = False
 
     def __init__(self, root):
+        """Define window for the app"""
         self.root = root
         self.root.geometry("600x400")
-        self.root.title("My notepad")
-        self.root['bg'] = "coral"
-        self.frame1 = tk.Frame(self.root)
-        self.frame1['bg'] = "coral"
-        self.frame1.pack(side="left", fill=tk.Y)
-        self.lb = tk.Listbox(self.frame1, width=50)
-        self.lb.pack(fill=tk.Y, expand=1)
-        for file in glob("*.txt"):
-            self.lb.insert(tk.END, file)
-        self.text = tk.Text(self.root, width=70)
-        self.text.pack(fill=tk.BOTH, expand=1)
+        self.root['bg'] = "white"
+        self.left_menu()
+        self.editor()
         self.root.bind("<F11>", lambda x: self.toggle_fullscreen())
-        self.lb.bind("<Double-Button>", lambda x: self.show_text())
+
+    # Widgets on the left =====================|
+    def left_menu(self):
+        """List of files in the folder"""
+        self.frame1 = tk.Frame(self.root)
+        self.frame1["bg"] = "black"
+        self.frame1.pack(side='left', fill=tk.Y)
+        self.button = tk.Button(self.frame1, text="Save", command=self.save)
+        self.lstb = tk.Listbox(self.frame1, width=50)
+        self.button.pack()
+        self.lstb['bg'] = "black"
+        self.lstb['fg'] = "gold"
+        self.lstb.pack(fill=tk.Y, expand=1)
+        self.insert_files()
+        self.lstb.bind("<<ListboxSelect>>", lambda x: self.show_text())
 
     def show_text(self):
-        num_item = self.lb.curselection()  # the index of item selected
-        fname = self.lb.get(num_item)  # the file name selected
-        with open(fname) as file:
+        """Shows text of selected file"""
+        if not self.lstb.curselection() == ():  #when you have selected an item
+            noi = self.lstb.curselection()[0]
+            self.filename = self.lstb.get(noi)
+            fn = self.opentext(self.filename)
             self.text.delete("1.0", tk.END)
+            self.text.insert(tk.END, fn)
+
+    def save(self):
+        with open(self.filename, "w", encoding="utf-8") as file:
+            file.write(self.text.get("1.0", tk.END))
+
+    def editor(self):
+        self.text = tk.Text(self.root)
+        self.text['bg'] = "white"
+        self.text['fg'] = "black"
+        self.text['font'] = "Arial 12"
+        self.text.pack(fill=tk.BOTH, expand=1)
+
+    def insert_files(self):
+        """Insert file names in the list on the left"""
+        files = glob.glob("*.txt")
+        for file in files:
+            self.lstb.insert(tk.END, file)
+
+    def opentext(self, fname):
+        with open(fname) as file:
             file = file.read()
-            self.text.insert(tk.END, file)
+        return file
 
     def toggle_fullscreen(self):
-        if Notepad.fullscreen == False:
+        if Ebook.fullscreen == False:
             self.root.attributes("-fullscreen", True)
-            Notepad.fullscreen = True
+            Ebook.fullscreen = True
         else:
             self.root.attributes("-fullscreen", False)
-            Notepad.fullscreen = False
+            Ebook.fullscreen = False
 
 
 root = tk.Tk()  # creates a window
-app = Notepad(root)
+app = Ebook(root)
+app.root.title("Ebook maker")
 root.mainloop()
